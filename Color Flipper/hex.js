62f363c4a7687ab1b4a63e080195be2ed320ca51
i252-hub <jushirarey@gmail.com>
2024-08-18 15:54:36 +0800

    created hex

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -1,1 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btns = document.querySelector("#btn");
+text = document.querySelector(".color");
+
+btns.addEventListener("click", () => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    text.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
